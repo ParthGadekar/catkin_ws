e89e81e3f75281563250164c9fc7282e08b3fad1
Spandan Ghosh <spandu500@gmail.com>
2020-10-17 20:16:02 +0530

    phix algo?

--- src/pkg_task0/scripts/node_turtle_revolve.py
+++ src/pkg_task0/scripts/node_turtle_revolve.py
@@ -4,13 +4,13 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
 def revolve():
-    radius = 1#float(input("Enter Radius :"))
-    speed = 1#float(input("Enter Speed :"))
-    distance = 2*3.14*radius
+    radius = 1 #float(input("Enter Radius :"))
+    speed = 1 #float(input("Enter Speed :"))
+    distance = 2*3.1415926535897*radius
     rospy.init_node('turtle_revolve', anonymous = True)  
     vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
-    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    rospy.Subscriber('/turtle1/pose', Pose )#pose_callback)
     rate = rospy.Rate(5)
     while not rospy.is_shutdown() : 
         vel_msg.linear.x = speed
@@ -18,16 +18,20 @@ def revolve():
         vel_msg.angular.z = speed/radius
         current_distance = 0
         t0 = rospy.Time.now().to_sec()
+        while(current_distance <= distance):
+            vel_pub.publish(vel_msg)
+            t1=rospy.Time.now().to_sec()
+            current_distance = speed*(t1-t0)
+            print current_distance
+            print "Turtle is revolving"
+        print"Turtle has stopped"
+        vel_msg.linear.x=0
+        vel_msg.linear.z=0
+        rospy.signal_shutdown('finished')
     rate.sleep()
 
-def pose_callback(vel):
-    while(vel.current_distance < vel.distance):
-        print vel.current_distance
-        print "Turtle is revolving"
-    print "Turtle has stopped"
-    vel_msg.linear.x=0
-    vel_msg.linear.z=0
-    rospy.signal_shutdown('end')
+#def pose_callback(vel):
+    
 
 if __name__=='__main__':
     try:
