48f13bda3301a1a1992eeaf677611dc585264a5e
Spandan Ghosh <spandu500@gmail.com>
2020-10-19 01:22:38 +0530

    make 69 algo changes

--- src/pkg_task0/scripts/task0.py
+++ src/pkg_task0/scripts/task0.py
@@ -6,31 +6,29 @@ from turtlesim.msg import Pose
 def revolve():
     radius = 1 #float(input("Enter Radius :"))
     speed = 1 #float(input("Enter Speed :"))
-    distance = 2*3.141592653589793238*radius
+    #raw_input("Press enter to continue") 
+    distance = 2*3.141592653589793238*radius#radius
     rospy.init_node('turtle_revolve', anonymous = True)  
     vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
-    rospy.Subscriber('/turtle1/pose', Pose )#pose_callback)
+    rospy.Subscriber('/turtle1/pose', Pose )
     rate = rospy.Rate(5)
-    while not rospy.is_shutdown() : 
-        vel_msg.linear.x = speed
-        vel_msg.linear.z = 0.0
-        vel_msg.angular.z = speed/radius
-        current_distance = 0
-        t0 = rospy.Time.now().to_sec()
-        while(current_distance < distance):
-            vel_pub.publish(vel_msg)
-            t1=rospy.Time.now().to_sec()
-            current_distance = speed*(t1-t0)
-            print current_distance
-            print "Turtle is revolving"
-        print"Turtle has stopped"
-        vel_msg.linear.x=0
-        vel_msg.linear.z=0
-        rospy.signal_shutdown('finished')
+    vel_msg.linear.x = speed
+    vel_msg.linear.z = 0.0
+    vel_msg.angular.z = speed/radius
+    current_distance = 0
+    t0 = rospy.Time.now().to_sec()
+    while(current_distance < distance):
+        vel_pub.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_distance = speed*(t1-t0)
+        print "Turtle is revolving   ",current_distance
+    print"Turtle has stopped    ",distance
+    vel_msg.linear.x=0
+    vel_msg.angular.z=0
     rate.sleep()
-
-#def pose_callback(vel):
+    raw_input("Press enter to continue") 
+    #rospy.spin()
     
 
 if __name__=='__main__':
--- src/pkg_task0/scripts/turt3.py
+++ src/pkg_task0/scripts/turt3.py
@@ -2,37 +2,37 @@
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+PI = 3.1415926535897
 
 def revolve():
-    radius = float(input("Enter Radius :"))
-    speed = float(input("Enter Speed :"))
-    distance = 2*3.14*radius
+
     rospy.init_node('turtle_revolve', anonymous = True)  
     vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
-    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
-    rate = rospy.Rate(5)
-    while not rospy.is_shutdown() : 
-        vel_msg.linear.x = speed
-        vel_msg.linear.z = 0.0
-        vel_msg.angular.z = speed/radius
-        current_distance = 0
-        t0 = rospy.Time.now().to_sec()
-        while(current_distance < distance):
-            vel_pub.publish(vel_msg)
-            t1=rospy.Time.now().to_sec()
-            current_distance = speed*(t1-t0)
-            print current_distance
-            print "Turtle is revolving"
-        print "Turtle has stopped"
-        vel_msg.linear.x=0
-        vel_msg.linear.z=0
-        rospy.signal_shutdown('finished')
-    rate.sleep()
+    speed=36 #degrees/sec
+    angle=360
+    angular_speed=speed*2*PI/360#radians/sec
+    radian_angle=angle*2*PI/360
+    rospy.Subscriber('/turtle1/pose', Pose)
+    rate = rospy.Rate(5) 
+    vel_msg.linear.x = 1.0
+    vel_msg.linear.z = 0.0
+    vel_msg.angular.z = abs(angular_speed)
+    current_angle = 0
+    t0 = rospy.Time.now().to_sec()
+    while(current_angle < radian_angle):
+        vel_pub.publish(vel_msg)
+        t1=rospy.Time.now().to_sec()
+        current_angle = angular_speed*(t1-t0)
+        print current_angle
+        print "Turtle is revolving"
+    print "Turtle has stopped"
+    vel_msg.linear.x=0
+    vel_msg.angular.z=0
+    rospy.signal_shutdown('finished')
+    rospy.spin()
+    #raw_input("Press enter to continue") 
 
-def pose_callback(vel):
-    while not (round(vel.theta)==0.0):
-        print vel.theta
     
 
 if __name__=='__main__':
