e8b80ade13ff7cbce2d3986207f53c76d943b76e
Spandan Ghosh <spandu500@gmail.com>
2020-10-31 21:20:09 +0530

    upgrade stuff

--- src/pkg_task0/scripts/task0.py
+++ src/pkg_task0/scripts/task0.py
@@ -7,12 +7,12 @@ def revolve():
     radius = 1 #float(input("Enter Radius :"))
     speed = 1 #float(input("Enter Speed :"))
     #raw_input("Press enter to continue") 
-    distance = 2*3.141592653589793238*radius#radius
+    distance = 2*3.141592653589793238*radius
     rospy.init_node('turtle_revolve', anonymous = True)  
     vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
     rospy.Subscriber('/turtle1/pose', Pose )
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(10)
     vel_msg.linear.x = speed
     vel_msg.linear.z = 0.0
     vel_msg.angular.z = speed/radius
@@ -23,14 +23,12 @@ def revolve():
         t1 = rospy.Time.now().to_sec()
         current_distance = speed*(t1-t0)
         print "Turtle is revolving   ",current_distance
-    print"Turtle has stopped    ",distance
     vel_msg.linear.x=0
     vel_msg.angular.z=0
-    rate.sleep()
-    raw_input("Press enter to continue") 
+    print"Turtle has stopped    ",distance
+    #raw_input("Press enter to continue") 
     #rospy.spin()
     
-
 if __name__=='__main__':
     try:
         revolve()
